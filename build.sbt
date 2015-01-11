d42b886e69bb1a72d299691345303c590d2d99ca
Rahel Lüthy <rahel.luethy@gmail.com>
2015-01-11 17:48:11 +0100

    First shot at stop watch

--- build.sbt
+++ build.sbt
@@ -4,10 +4,12 @@ name := "hot-wire-scala-js"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.2"
 
 ScalaJSKeys.persistLauncher := true
+skip in ScalaJSKeys.packageJSDependencies := false
 
 libraryDependencies ++= Seq(
-    "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
+    "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6",
+    "org.scala-lang.modules.scalajs" %%% "scalajs-jquery" % "0.6"
 )
--- src/main/scala/ch/netzwerg/HotWire.scala
+++ src/main/scala/ch/netzwerg/HotWire.scala
@@ -3,6 +3,7 @@ package ch.netzwerg
 import org.scalajs.dom
 import org.scalajs.dom.KeyboardEvent
 import org.scalajs.dom.extensions.{Color, KeyCode}
+import org.scalajs.jquery.jQuery
 
 import scala.scalajs.js
 
@@ -11,9 +12,11 @@ object HotWire extends js.JSApp {
   val DarkGreen = Color("#006400")
   val DarkRed = Color("#8B0000")
 
+  var startTime = js.Date.now()
+
   def main() {
     init()
-    dom.onkeypress = {(e: KeyboardEvent) =>
+    dom.onkeypress = { (e: KeyboardEvent) =>
       if (e.keyCode == KeyCode.enter) {
         reset()
       } else {
@@ -24,9 +27,19 @@ object HotWire extends js.JSApp {
 
   def init() {
     reset()
+    dom.setInterval(() => jQuery("#stopwatch").text(elapsedTimeFormatted()), 15)
+  }
+
+  def elapsedTimeFormatted() = {
+    val elapsed: Long = (js.Date.now() - startTime).toLong
+    val mins = (elapsed / (1000 * 60)) % 60
+    val secs = (elapsed / 1000) % 60
+    val millis = elapsed % 1000
+    "%02d:%02d:%03d".format(mins, secs, millis)
   }
 
   def reset() {
+    startTime = js.Date.now()
     setBgColor(DarkGreen)
   }
 
